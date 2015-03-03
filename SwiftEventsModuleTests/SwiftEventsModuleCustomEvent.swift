f8c0a8d813ca3ae62a2c7110237a1a1386a23d02
Philip Georgiev <philip.georgiev@productmadness.com>
2015-03-03 19:25:56 +0000

    [DEV] remove class that not needed in framework file

---
+++ SwiftEventsModuleTests/SwiftEventsModuleCustomEvent.swift
@@ -0,0 +1,44 @@
+//
+//  SwiftEventsModuleCustomEvent.swift
+//  SwiftEventsModule
+//
+//  Created by freezing on 03/03/2015.
+//  Copyright (c) 2015 iccode. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SwiftEventsModuleCustomEvent: XCTestCase {
+    var event : CustomEvent?
+    var eventHandled : CustomEvent?
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        self.event = CustomEvent(attribute: "test_some_custom_data")
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        var view : EventDispatcher = EventDispatcher()
+        view.addEventListener(CustomEvent.CUSTOM_TYPE, withFunction: handleCustomEvent)
+        view.dispatchEvent(self.event!)
+        XCTAssert(self.event === self.eventHandled, "couldn't fetch custom event")
+    }
+    
+    func handleCustomEvent(e : Event)
+    {
+        var c : CustomEvent = e as CustomEvent;
+        self.eventHandled = c
+    }
+
+}
+
+
